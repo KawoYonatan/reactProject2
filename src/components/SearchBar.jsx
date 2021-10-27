b058915457734033f74d08f1065d72908386bdec
YonatanKawo <yonatankawo@gmail.com>
2021-10-27 09:10:08 -0500

    reactProje

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+// import React, {useState} from "react";
+// import SearchIcon from '@mui/icons-material/Search';
+
+// export default function SearchBar({ jokes }) {
+
+//     const [SearchJokes, setSearchJokes] = useState('');
+
+//     const handleSearch = (event) => {
+//         setSearchJokes(event.target.value)
+//     }
+
+//     const 
+//     return (
+
+//         <div className="searchBar">
+//             <input
+//             type="text"
+//             placeholder= "search..."
+//             onChange={handleSearch}
+//             />
+//             <div className="searchIcon">SearchIcon</div>
+//         </div>
+
+//     );
+// }
